b35ac2ae7977b69471ac2480d82d7ce8b5d2ed5a
joao <joaoh10cabral@gmail.com>
2025-11-28 09:16:59 -0300

    Primeiro teste com a interface pronta

--- Conversor/MainWindow.xaml.cs
+++ Conversor/MainWindow.xaml.cs
@@ -1,31 +1,18 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+using Conversor.ViewModels;
 
 namespace Conversor
 {
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainWindow : Window
     {
+        public MainViewModel ViewModel { get; }
+        public MainViewModel DataContext { get; }
+
         public MainWindow()
         {
-            InitializeComponent();
+            ViewModel = new MainViewModel();
+            this.InitializeComponent();
+            this.DataContext = ViewModel;
         }
     }
 }
