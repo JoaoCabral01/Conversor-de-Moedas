333ee7d56175757ff9b6ac241b6b11d9bd68e8a5
joao <joaoh10cabral@gmail.com>
2025-11-28 08:10:19 -0300

    Adiconei o RelayCommad para gerenciar os botões no padrão MVVM

--- Conversor/Helpers/RelayCommand.cs
+++ Conversor/Helpers/RelayCommand.cs
@@ -1,12 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Input;
 
-namespace Conversor.Helpers
+namespace  conversor.Helpers
 {
-    internal class RelayCommand
+    public class RelayCommand : ICommand
     {
+        private readonly Action<object?> _execute;
+        private readonly Func<object?, bool>? _canExecute;
+        public event EventHandler? CanExecuteChanged;
+        public RelayCommand(Action<object?> execute, Func<object?, bool>? canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+        public bool CanExecute(object? parameter) => _canExecute?.Invoke(parameter) ?? true;
+        public void Execute(object? parameter) => _execute(parameter);
+        public void LevantarMudanca() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
     }
 }
+
